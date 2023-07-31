dec37246083f3facb11a728d5b18f82cc40e37db
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-07-31 17:36:56 -0300

    Mover verificação de repositorio para NewBaseHandler

--- backend/controllers/baseHandler.go
+++ backend/controllers/baseHandler.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"log"
 	"orb-api/config"
-
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,15 +11,15 @@ type BaseHandler struct {
 }
 
 func NewBaseHandler(repository *config.Repository) *BaseHandler {
+  if repository == nil {
+    log.Fatal("Missing Database connection")
+  }
+
   return &BaseHandler{
     Repository: repository,
   }
 }
 
 func (handler *BaseHandler) HandleHello(context *fiber.Ctx) error {
-  if handler.Repository.DB == nil {
-    log.Fatal("Missing Database connection")
-  }
-
   return context.SendString("Orb API is running")
 } 
