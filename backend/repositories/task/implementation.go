8e256e9b4f0453c4596a17868169151414888c68
Gustavo Costa <gustavo1204.gc@gmail.com>
2023-08-15 18:26:49 -0300

    Adicionar repositorio de tarefas no repositorio principal

--- backend/repositories/task/implementation.go
+++ backend/repositories/task/implementation.go
@@ -2,9 +2,9 @@ package task
 
 import (
 	"errors"
+	"gorm.io/gorm"
 	"orb-api/models"
 	"time"
-	"gorm.io/gorm"
 )
 
 func NewTaskRepository(db *gorm.DB) Repository {
@@ -69,106 +69,108 @@ func (r *Repository) Create(createData ICreate) error {
 }
 
 func (r *Repository) ReadAll() ([]models.Task, error) {
-  var taskArray []models.Task
+	var taskArray []models.Task
 
-  result := r.GetDB().Find(&taskArray)
+	result := r.GetDB().Find(&taskArray)
 
-  if result.Error != nil {
-    return nil, result.Error
-  }
+	if result.Error != nil {
+		return nil, result.Error
+	}
 
-  return taskArray, nil
+	return taskArray, nil
 }
 
 func (r *Repository) ReadBy(readby IReadBy) ([]models.Task, error) {
-  var fieldMap map[string]interface{} 
-  var taskArray []models.Task  
-  var result *gorm.DB
+	var fieldMap map[string]interface{}
+	var taskArray []models.Task
+	var result *gorm.DB
 
-  if readby.ID == nil && 
-    readby.AssignedTo == nil &&
-    readby.CreatedBy == nil && 
-    readby.Status == nil && 
-    readby.TimeRange == nil {
-    return nil, errors.New("No fields to read")
-  }
-  
-  if readby.ID != nil {
-    fieldMap["ID"] = *readby.ID
-  }
+	if readby.ID == nil &&
+		readby.AssignedTo == nil &&
+		readby.CreatedBy == nil &&
+		readby.Status == nil &&
+		readby.TimeRange == nil {
+		return nil, errors.New("No fields to read")
+	}
 
-  if readby.AssignedTo != nil {
-    fieldMap["AssignedTo"] = *readby.AssignedTo
-  }
+	if readby.ID != nil {
+		fieldMap["ID"] = *readby.ID
+	}
 
-  if readby.CreatedBy != nil {
-    fieldMap["CreatedBy"] = *readby.CreatedBy
-  }
+	if readby.AssignedTo != nil {
+		fieldMap["AssignedTo"] = *readby.AssignedTo
+	}
 
-  if readby.Status != nil {
-    if !ValidTaskStatus(*readby.Status) {
-      return nil, errors.New("Invalid task status")
-    }
+	if readby.CreatedBy != nil {
+		fieldMap["CreatedBy"] = *readby.CreatedBy
+	}
 
-    fieldMap["AssignedTo"] = *readby.Status
-  }
+	if readby.Status != nil {
+		if !ValidTaskStatus(*readby.Status) {
+			return nil, errors.New("Invalid task status")
+		}
 
-  if readby.TimeRange != nil {
-    result = r.GetDB().Where(fieldMap, "deadline <= ?", *readby.TimeRange).Find(&taskArray)
-  } else {
-    result = r.GetDB().Where(fieldMap).Find(&taskArray)
-  }
+		fieldMap["AssignedTo"] = *readby.Status
+	}
 
-  if result.Error != nil {
-    return nil, result.Error
-  }
+	if readby.TimeRange != nil {
+		result = r.GetDB().Where(fieldMap, "deadline <= ?", *readby.TimeRange).Find(
+			&taskArray,
+		)
+	} else {
+		result = r.GetDB().Where(fieldMap).Find(&taskArray)
+	}
 
-  return nil, nil
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return nil, nil
 }
 
 func (r *Repository) Update(updateData IUpdate) error {
- 	var createdBy = models.User{ID: *updateData.CreatedBy}
+	var createdBy = models.User{ID: *updateData.CreatedBy}
 	var assignedTo = models.User{ID: *updateData.AssignedTo}
-  var task = models.Task{ID: updateData.ID}
-  var fieldMap map[string]interface{}
- 
-  if updateData.Description == nil && 
-    updateData.AssignedTo == nil &&
-    updateData.CreatedBy == nil && 
-    updateData.Status == nil && 
-    updateData.Deadline == nil {
-    return errors.New("No fields to read")
-  }
-  
-  if updateData.Description != nil {
-    fieldMap["Description"] = *updateData.Description
-  }
+	var task = models.Task{ID: updateData.ID}
+	var fieldMap map[string]interface{}
 
-  if updateData.AssignedTo != nil {
-    fieldMap["AssignedTo"] = *updateData.AssignedTo
-  } 
+	if updateData.Description == nil &&
+		updateData.AssignedTo == nil &&
+		updateData.CreatedBy == nil &&
+		updateData.Status == nil &&
+		updateData.Deadline == nil {
+		return errors.New("No fields to read")
+	}
 
-  if updateData.CreatedBy != nil {
-    fieldMap["CreatedBy"] = *updateData.CreatedBy
-  }
+	if updateData.Description != nil {
+		fieldMap["Description"] = *updateData.Description
+	}
 
-  if updateData.Status != nil {
-    if !ValidTaskStatus(*updateData.Status) {
-      return errors.New("Invalid task status")
-    }
+	if updateData.AssignedTo != nil {
+		fieldMap["AssignedTo"] = *updateData.AssignedTo
+	}
 
-    fieldMap["Status"] = *updateData.Status
-  }
+	if updateData.CreatedBy != nil {
+		fieldMap["CreatedBy"] = *updateData.CreatedBy
+	}
 
-  if updateData.Deadline != nil {
-    if !ValidDeadline(*updateData.Deadline) {
-      return errors.New("Invalid deadline")
-    }
+	if updateData.Status != nil {
+		if !ValidTaskStatus(*updateData.Status) {
+			return errors.New("Invalid task status")
+		}
 
-    fieldMap["Deadline"] = *updateData.Deadline
-  }
-  
-  verifyCreateBy := r.GetDB().First(&createdBy)
+		fieldMap["Status"] = *updateData.Status
+	}
+
+	if updateData.Deadline != nil {
+		if !ValidDeadline(*updateData.Deadline) {
+			return errors.New("Invalid deadline")
+		}
+
+		fieldMap["Deadline"] = *updateData.Deadline
+	}
+
+	verifyCreateBy := r.GetDB().First(&createdBy)
 
 	if verifyCreateBy.Error != nil {
 		return verifyCreateBy.Error
@@ -180,15 +182,15 @@ func (r *Repository) Update(updateData IUpdate) error {
 		return verifyAssignedTo.Error
 	}
 
-  fieldMap["UpdatedAt"] = time.Now()
+	fieldMap["UpdatedAt"] = time.Now()
 
-  result := r.GetDB().Model(&task).Updates(fieldMap)
+	result := r.GetDB().Model(&task).Updates(fieldMap)
 
-  if result.Error != nil {
-    return result.Error
-  }
+	if result.Error != nil {
+		return result.Error
+	}
 
-  return nil
+	return nil
 }
 
 func (r *Repository) Delete(deleteData IDelete) error {
