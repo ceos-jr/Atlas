7bf49f828f9e0852c26cd2bf6c380869dcec2cd3
unknown <marcelomeirelesmarquesfilho@hotmail.com>
2023-08-18 10:48:20 -0300

    Configurar setup do teste dos cargos

---
+++ backend/tests/unit/role_repository/setup.go
@@ -0,0 +1,38 @@
+package rolerepotest
+
+import (
+	"orb-api/config"
+	"orb-api/models"
+	repository "orb-api/repositories"
+	"orb-api/repositories/role"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type RoleRepoTestSuite struct {
+	suite.Suite
+	Repo      *repository.Repository
+	MockRoles []models.Role
+}
+
+func (suite *RoleRepoTestSuite) SetupSuite() {
+	repo, setupError := config.SetupDB()
+
+	if setupError != nil {
+		panic(setupError)
+	}
+
+	suite.Repo = repo
+}
+
+func (suite *RoleRepoTestSuite) TearDownSuite() {
+	for index := range suite.MockRoles {
+		_, deleteErr := suite.Repo.Role.Delete(role.IDelete{
+			RoleID: suite.MockRoles[index].ID,
+		})
+
+		if deleteErr != nil {
+			panic(deleteErr)
+		}
+	}
+}
