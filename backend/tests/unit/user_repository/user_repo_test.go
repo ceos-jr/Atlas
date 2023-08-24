399c323842b6d214f8442e626a9ac1e0726b6f78
marcelomx30 <103221027+marcelomx30@users.noreply.github.com>
2023-08-24 12:24:19 +0000

    Ajeitar CreateUser

--- backend/tests/unit/user_repository/user_repo_test.go
+++ backend/tests/unit/user_repository/user_repo_test.go
@@ -3,24 +3,25 @@ package userrepotest
 import (
 	"orb-api/repositories/user"
 	"testing"
+
 	"github.com/stretchr/testify/suite"
 )
 
 func (suite *UserRepoTestSuite) TestCreateUser() {
 	user, createErr := suite.Repo.User.Create(user.ICreate{
-		Name:    "User 01",
-		Email:  "user01@example.com",
-		Password: "123456",
-		Status:  1,
-})
+		Name:     "User 01",
+		Email:    "user01@example.com",
+		Password: "12345678",
+		Status:   1,
+	})
 
 	suite.Nil(createErr, "Create error must be nil")
 	suite.Equal("User 01", user.Name, "Name does not match")
 	suite.Equal("user01@example.com", user.Email, "Email does not match")
 	suite.Equal(uint(1), user.Status, "Status does not match")
-	suite.Equal("123456", user.Password, "Password does not match")
+	suite.Equal("12345678", user.Password, "Password does not match")
 
-	suite.MockUsers[0] = *user
+	suite.MockUsers = append(suite.MockUsers, *user)
 }
 
 func (suite *UserRepoTestSuite) TestReadAllUsers() {
@@ -34,7 +35,7 @@ func (suite *UserRepoTestSuite) TestReadAllUsers() {
 func (suite *UserRepoTestSuite) TestReadUserByID() {
 	users, readErr := suite.Repo.User.ReadBy(user.IReadBy{
 		ID: &suite.MockUsers[0].ID,
-})
+	})
 
 	suite.Nil(readErr, "Read error must be nil")
 	suite.Equal(1, len(users), "Expected to have one user")
@@ -44,29 +45,28 @@ func (suite *UserRepoTestSuite) TestReadUserByID() {
 func (suite *UserRepoTestSuite) TestReadUserByName() {
 	users, readErr := suite.Repo.User.ReadBy(user.IReadBy{
 		Name: &suite.MockUsers[0].Name,
-})
+	})
 
 	suite.Nil(readErr, "Read error must be nil")
 	suite.Equal(1, len(users), "Expected to have one user")
 	suite.Equal(suite.MockUsers[0].ID, users[0].ID, "Expected to have the same ID")
-	
+
 }
 
 func (suite *UserRepoTestSuite) TestReadUserByEmail() {
 	users, readErr := suite.Repo.User.ReadBy(user.IReadBy{
 		Email: &suite.MockUsers[0].Email,
-})
+	})
 
 	suite.Nil(readErr, "Read error must be nil")
 	suite.Equal(1, len(users), "Expected to have one user")
 	suite.Equal(suite.MockUsers[0].ID, users[0].ID, "Expected to have the same ID")
 }
 
-
 func (suite *UserRepoTestSuite) TestReadUserByStatus() {
 	users, readErr := suite.Repo.User.ReadBy(user.IReadBy{
 		Status: &suite.MockUsers[0].Status,
-})
+	})
 
 	suite.Nil(readErr, "Read error must be nil")
 	suite.Equal(suite.MockUsers[0].ID, users[0].ID, "Expected to have the same ID")
@@ -77,7 +77,7 @@ func (suite *UserRepoTestSuite) TestReadByErr() {
 	_, readErr := suite.Repo.User.ReadBy(user.IReadBy{})
 
 	suite.Equal("No fields to read", readErr.Error(), "Expected to have fields error")
-	
+
 }
 
 func TestUserRepository(t *testing.T) {
@@ -90,12 +90,12 @@ func (suite *UserRepoTestSuite) TestUpdateUser() {
 	name := "Iguinho"
 	email := "iguinho@email.com"
 	status := uint(3)
-	
+
 	updatedUser, updateError := suite.Repo.User.Update(user.IUpdate{
-		ID:          suite.MockUsers[1].ID,
-		Name:        &name,
-		Email:       &email,
-		Status:      &status,
+		ID:     suite.MockUsers[1].ID,
+		Name:   &name,
+		Email:  &email,
+		Status: &status,
 	})
 
 	suite.Nil(updateError, "Update error must be nil")
@@ -110,9 +110,17 @@ func (suite *UserRepoTestSuite) TestUpdateUser() {
 	)
 }
 
+func GenerateString(length int) string {
+	var generatedString = ""
+
+	for i := 0; i < length; i++ {
+		generatedString += "L"
+	}
+	return generatedString
+}
 func (suite *UserRepoTestSuite) TestUpdateUserErr() {
-	invalidName := string(128)
-	invalidEmail := string(128)
+	invalidName := GenerateString(129)
+	invalidEmail := GenerateString(129)
 	invalidStatus := uint(77)
 
 	// Teste 1: Tentativa de atualizar sem campos
@@ -123,22 +131,21 @@ func (suite *UserRepoTestSuite) TestUpdateUserErr() {
 	suite.Equal("No fields to update", updateError.Error(),
 		"Empty fields it should return an error",
 	)
-	
+
 	// Teste 2: Tentativa de atualizar com status inválido
 	_, updateError = suite.Repo.User.Update(user.IUpdate{
 		ID:     suite.MockUsers[1].ID,
 		Status: &invalidStatus,
 	})
 
-	
 	suite.Equal("Invalid user status", updateError.Error(),
 		"Invalid user status it should return an error",
 	)
-	
+
 	// Teste 3: Tentativa de atualizar com nome inválido
 	_, updateError = suite.Repo.User.Update(user.IUpdate{
-		ID:       suite.MockUsers[1].ID,
-		Name: 	  &invalidName,
+		ID:   suite.MockUsers[1].ID,
+		Name: &invalidName,
 	})
 
 	suite.Equal("Invalid name", updateError.Error(),
@@ -147,8 +154,8 @@ func (suite *UserRepoTestSuite) TestUpdateUserErr() {
 
 	// Teste 4: Tentativa de atualizar com email inválido
 	_, updateError = suite.Repo.User.Update(user.IUpdate{
-		ID:        suite.MockUsers[1].ID,
-		Email: 	   &invalidEmail,
+		ID:    suite.MockUsers[1].ID,
+		Email: &invalidEmail,
 	})
 
 	suite.Equal("Invalid user passed to createBy", updateError.Error(),
@@ -157,15 +164,12 @@ func (suite *UserRepoTestSuite) TestUpdateUserErr() {
 
 }
 
-
-
 func (suite *UserRepoTestSuite) TestDeleteUser() {
 	newUser, _ := suite.Repo.User.Create(user.ICreate{
-		Name: 		 "vanilla",
-		Email: 		 "vanilla@email.com",
-		Status:      1,
-		Password:    "123454",
-		
+		Name:     "vanilla",
+		Email:    "vanilla@email.com",
+		Status:   1,
+		Password: "123454",
 	})
 
 	deletedUser, deleteErr := suite.Repo.User.Delete(user.IDelete{
