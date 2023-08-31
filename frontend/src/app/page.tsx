308f432170532311ecced744e6404ea71a6a15ba
Lucas Braide <107264197+lucasbraide@users.noreply.github.com>
2023-08-31 19:07:52 -0300

    front-end configurado

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import React from 'react';
+import LoginPage from './login/page';
+import { useRouter } from 'next/navigation';
+
+const Home: React.FC = () => {
+  const router = useRouter();
+
+  const goToLoginPage = () => {
+    router.push('/login');
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen text-center font-quick">
+        <div className="card bg-white rounded-lg shadow-md p-6">
+          <h1 className="text-3xl font-semibold mb-4 text-primary-blue">Boas vindas ao Atlas! </h1>
+          <p className="text-lg text-gray-600"> Uma plataforma para gerenciamento de demandas e equipes. </p>
+          <button onClick={goToLoginPage} className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300"> Login </button>
+        </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,29 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'primary-blue': '#2860FF',
+        'secondary-blue': '#2196F2',
+        'primary-orange': '#FF9100',
+        'primary-yellow': '#FFC107'
+      },
+      fontFamily: {
+        'quick' : ['Quicksand', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+export default config
