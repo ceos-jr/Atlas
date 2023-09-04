bed497c5a39cd249fd32b9c19930fb9f8d74a69e
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-03 21:36:59 -0300

    Formatar codigo

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,27 +1,33 @@
-"use client"
+"use client";
 
-import React, { ReactNode } from 'react';
-import { useRouter } from 'next/navigation';
-
-
+import React from "react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
   const router = useRouter();
 
   const goHomePage = () => {
-    router.push('/');
+    router.push("/");
   };
 
   return (
     <div className="flex justify-center items-center h-screen text-center font-quick">
       <div className="card bg-white rounded-lg shadow-md p-6">
-          <h1 className="text-3xl font-semibold mb-4 text-primary-blue">Teste Login</h1>
-          <p className="text-lg text-gray-600"> Uma plataforma para gerenciamento de demandas e equipes. </p>
-          <button onClick = {goHomePage}className='className="px-4 py-2 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-xs"'> Home </button>
+        <h1 className="text-3xl font-semibold mb-4 text-primary-blue">
+          Teste Login
+        </h1>
+        <p className="text-lg text-gray-600">
+          Uma plataforma para gerenciamento de demandas e equipes.
+        </p>
+        <button
+          onClick={goHomePage}
+          className='className="px-4 py-2 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-xs"'
+        >
+          Home
+        </button>
       </div>
     </div>
   );
 };
-
 
 export default LoginPage;
