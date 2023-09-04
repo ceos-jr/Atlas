eca725866e41088ff0dbc5ae6f614f72b67b2fe6
Lu1z-Gust4v0 <gustavo1204.gc@gmail.com>
2023-09-03 22:43:14 -0300

    Definir interface para o service de usuario

---
+++ backend/services/user/interface.go
@@ -0,0 +1,30 @@
+package userservice
+
+import (
+	"orb-api/models"
+	"orb-api/repositories/user"
+)
+
+const (
+	emailMaxLen    = 128
+	emailMinLen    = 3
+	nameMaxLen     = 128
+	nameMinLen     = 5
+	passwordMinLen = 8
+)
+
+type (
+    UserService struct {
+	    UserRepo user.Repository
+    }        
+   
+    ICreateUser struct {
+        Name     string 
+        Email    string     
+        Password string
+    }
+        
+    Interface interface {
+        CreateNewUser(ICreateUser) (*models.User, error)  
+    }
+)
