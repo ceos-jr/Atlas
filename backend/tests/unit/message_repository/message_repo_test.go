ac85d7d7be662771152e6bd4d448d6b0d8936d1c
viquecastro <victoriacastro3108@gmail.com>
2023-09-04 21:51:57 -0300

    Adicionar funções de update e delete nos testes

--- backend/tests/unit/message_repository/message_repo_test.go
+++ backend/tests/unit/message_repository/message_repo_test.go
@@ -1,90 +1,135 @@
 package messagerepotest
 
 import (
-	"orb-api/repositories/message"
-	"testing"
-
-	"github.com/stretchr/testify/suite"
+    "orb-api/repositories/message"
+    "testing"
+    
+    "github.com/stretchr/testify/suite"
 )
 
 func (suite *MessageRepoTestSuite) TestCreateMessage() {
+    message, createErr := suite.Repo.Message.Create(message.ICreate{
 
-	message, createErr := suite.Repo.Message.Create(message.ICreate{
-		Sender:   suite.MockUsers[0].ID,
-		Receiver: suite.MockUsers[1].ID,
-		Content:  "This is a message",
-	})
+        Sender:   suite.MockUsers[0].ID,
+        Receiver: suite.MockUsers[1].ID,
+        Content:  "This is a message",
+    })
 
-	suite.Nil(createErr, "Create error must be nil")
-	suite.Equal(suite.MockUsers[0].ID, message.Sender)
-	suite.Equal(suite.MockUsers[1].ID, message.Receiver)
-	suite.Equal("This is a message", message.Content)
+    suite.Nil(createErr, "Create error must be nil")
+    suite.Equal(suite.MockUsers[0].ID, message.Sender)
+    suite.Equal(suite.MockUsers[1].ID, message.Receiver)
+    suite.Equal("This is a message", message.Content)
 
 }
 
 func (suite *MessageRepoTestSuite) TestCreateMessageErr() {
-	invalidSender := 9999
-	invalidReceiver := 9999
-	invalidContent := ""
+    invalidSender := 9999
+    invalidReceiver := 9999
+    invalidContent := ""
 
-	_, createErr := suite.Repo.Message.Create(message.ICreate{
-		Sender:   uint(invalidSender),
-		Receiver: suite.MockUsers[1].ID,
-		Content:  "This is a message",
-	})
+    _, createErr := suite.Repo.Message.Create(message.ICreate{
+        Sender:   uint(invalidSender),
+        Receiver: suite.MockUsers[1].ID,
+        Content:  "This is a message",
+    })
 
-	suite.Equal("Invalid sender", createErr.Error(), "Expected to have an error")
+    suite.Equal("Invalid sender", createErr.Error(), "Expected to have an error")
 
-	_, createErr = suite.Repo.Message.Create(message.ICreate{
-		Sender:   suite.MockUsers[0].ID,
-		Receiver: uint(invalidReceiver),
-		Content:  "This is a message",
-	})
+    _, createErr = suite.Repo.Message.Create(message.ICreate{
+        Sender:   suite.MockUsers[0].ID,
+        Receiver: uint(invalidReceiver),
+        Content:  "This is a message",
+    })
 
-	suite.Equal("Invalid receiver", createErr.Error(), "Expected to have an error")
+    suite.Equal("Invalid receiver", createErr.Error(), "Expected to have an error")
 
-	_, createErr = suite.Repo.Message.Create(message.ICreate{
-		Sender:   suite.MockUsers[0].ID,
-		Receiver: suite.MockUsers[1].ID,
-		Content:  invalidContent,
-	})
+    _, createErr = suite.Repo.Message.Create(message.ICreate{
+        Sender:   suite.MockUsers[0].ID,
+        Receiver: suite.MockUsers[1].ID,
+        Content:  invalidContent,
+    })
 
-	suite.Equal("Content empty or too long", createErr.Error(), "Expected to have an error")
+    suite.Equal("Content empty or too long", createErr.Error(), "Expected to have an error")
 
-	_, createErr = suite.Repo.Message.Create(message.ICreate{
-		Sender:   suite.MockUsers[0].ID,
-		Receiver: suite.MockUsers[0].ID,
-		Content:  "This is a message",
-	})
+    _, createErr = suite.Repo.Message.Create(message.ICreate{
+        Sender:   suite.MockUsers[0].ID,
+        Receiver: suite.MockUsers[0].ID,
+        Content:  "This is a message",
+    })
 
-	suite.Equal("Can't send message to self", createErr.Error(),
-		"Expected to have an error",
-	)
+    suite.Equal("Can't send message to self", createErr.Error(),
+        "Expected to have an error",
+    )
 }
 
 func (suite *MessageRepoTestSuite) TestReadMessageBySender() {
-	messages, readErr := suite.Repo.Message.ReadBySender(message.IReadBySender{
-		Sender: suite.MockUsers[0].ID,
-	})
+    messages, readErr := suite.Repo.Message.ReadBySender(message.IReadBySender{
+        Sender: suite.MockUsers[0].ID,
+    })
 
-	suite.Nil(readErr, "Read Error must be nil")
-	suite.Equal(suite.MockUsers[0].ID, messages[0].Sender, "IDs must match")
+    suite.Nil(readErr, "Read Error must be nil")
+    suite.Equal(suite.MockUsers[0].ID, messages[0].Sender, "IDs must match")
 
 }
 
 func (suite *MessageRepoTestSuite) TestReadMessageByReceiver() {
-	messages, readErr := suite.Repo.Message.ReadByReceiver(message.IReadByReceiver{
-		Receiver: suite.MockUsers[1].ID,
-	})
+    messages, readErr := suite.Repo.Message.ReadByReceiver(message.IReadByReceiver{
+        Receiver: suite.MockUsers[1].ID,
+    })
 
-	suite.Nil(readErr, "Read Error must be nil")
-	suite.Equal(suite.MockUsers[1].ID, messages[0].Receiver, "IDs must match")
+    suite.Nil(readErr, "Read Error must be nil")
+    suite.Equal(suite.MockUsers[1].ID, messages[0].Receiver, "IDs must match")
 }
 
 //func (suite *MessageRepoTestSuite) TestReadChat() {
-//	messages, readErr := suite.R
+//  messages, readErr := suite.R
 //}
 
 func TestMessageRepository(test *testing.T) {
-	suite.Run(test, new(MessageRepoTestSuite))
+    suite.Run(test, new(MessageRepoTestSuite))
+}
+
+func (suite *MessageRepoTestSuite) TestUpdateMessage() {
+    content := "edited message"
+
+    updatedMessage, updateError := suite.Repo.Message.Update(message.IUpdate{
+        ID:        suite.MockMessages[1].ID,
+        Content:   content,
+    })
+
+    suite.Nil(updateError, "Update error must be nil")
+    suite.Equal(content, updatedMessage.Content, "Content do not match",)
+    suite.Equal(suite.MockUsers[1].ID, updatedMessage.Sender, "Sender do not match",)
+    suite.Equal(suite.MockUsers[0].ID, updatedMessage.Receiver, "Receiver do not match",)
+
+}
+
+// unchanged content = suite.MockMessages[1].Content,
+func (suite *MessageRepoTestSuite) TestUpdateMessageErr() {
+    invalidContent := ""
+
+    _, updateError := suite.Repo.Message.Update(message.IUpdate{
+        ID:      suite.MockUsers[0].ID,
+        Content: invalidContent, 
+    })
+
+    suite.Equal("No fields to update", updateError.Error(),
+        "Empty fields should return an error",
+    )
+}
+
+func (suite *MessageRepoTestSuite) TestDeleteMessage() {
+    newMessage, _ := suite.Repo.Message.Create(message.ICreate{
+        Content:    "Delete message test",
+        Sender:     suite.MockUsers[0].ID,
+        Receiver:   suite.MockUsers[1].ID,
+    })
+
+    deletedMessage, deleteErr := suite.Repo.Message.Delete(message.IDelete{
+        ID: newMessage.ID,
+    })
+
+    suite.Nil(deleteErr, "Delete error must be nil")
+    suite.Equal(newMessage, deletedMessage.ID, "Expected to have the same ID")
+
 }
