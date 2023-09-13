c15864db12d2aec3743b7d55b48d118fb69ffe38
Marcelo <marcelomeirelesmarquesfilho@hotmail.com>
2023-09-13 10:57:21 -0300

    Criação da interface do service role

---
+++ backend/service/role/interface.go
@@ -0,0 +1,36 @@
+package roleservice
+
+import (
+	"orb-api/models"
+	"orb-api/repositories/role"
+)
+
+type (
+	RoleService struct {
+		RoleRepo role.Repository
+	}
+
+ICreateRole{
+	Name string 
+	Descripton string
+}
+
+IUpdateName struct{
+	ID uint
+	Name string
+}
+
+IUpdateDescription struct{
+	ID uint
+	Description string
+}
+
+
+
+Interface interface {
+	CreateNewRole(ICreateRole) (*models.Role, error)
+	UpdateRoleName(id uint, name string) (*models.Role, error)
+	UpdateRoleDescription(id uint, description string) (*models.Role, error)
+}
+
+)
