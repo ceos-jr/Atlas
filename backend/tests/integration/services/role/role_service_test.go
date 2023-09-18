61af091854ae0004cc7e5cca738ff602a4650582
Marcelo <marcelomeirelesmarquesfilho@hotmail.com>
2023-09-18 10:06:24 -0300

    Simplificar  nome das funções

--- backend/tests/integration/services/role/role_service_test.go
+++ backend/tests/integration/services/role/role_service_test.go
@@ -20,13 +20,13 @@ func (suite *RoleServiceTestSuite) TestCreateRole() {
 
 }
 
-func (suite *RoleServiceTestSuite) TestUpdateRoleName() {
+func (suite *RoleServiceTestSuite) TestUpdateName() {
 	role, updateErr := suite.Service.UpdateRoleName(suite.MockRoles[0].ID, "NewRole")
 	suite.Nil(updateErr, "Update error must be nil")
 	suite.Equal("NewRole", role.Name, "Name does not match")
 }
 
-func (suite *RoleServiceTestSuite) TestUpdateRoleDescription() {
+func (suite *RoleServiceTestSuite) TestUpdateDescription() {
 	role, updateErr := suite.Service.UpdateRoleDescription(suite.MockRoles[0].ID, "New Description")
 	suite.Nil(updateErr, "Update error must be nil")
 	suite.Equal("New Description", role.Description, "Description does not match")
