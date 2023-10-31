1bfd8f599a2054bcf3e3a1ec6aac6d7ed32907f5
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2023-10-30 21:35:16 -0300

    Criar interface de projetos

---
+++ backend/repositories/project/interface.go
@@ -0,0 +1,47 @@
+package project
+
+import (
+	"gorm.io/gorm"
+	"orb-api/models"
+)
+
+type(
+	Repository struct{
+		Get func() *gorm.DB
+	}
+
+	ICreate struct{
+		Name		string
+		Sector		uint
+		AdmID		uint
+	}
+
+	IReadAll struct{
+		Limit		*uint
+	}
+
+	IReadBy struct{
+		ID		*uint
+		Name		*string
+		Sector		*uint
+		AdmID		*uint
+		Limit		*uint
+	}
+	IUpdate struct{
+		ID 		uint
+		Name 		*string
+		Sector	*uint
+	}
+
+	IDelete struct{
+		ID uint
+	}
+
+	Interface interface{
+		Create(ICreate)		(*models.Project, error)
+		ReadAll(IReadAll)	([]models.Project, error)
+		ReadBy(IReadBy)		([]models.Project, error)
+		Update(IUpdate)		(*models.Project, error)
+		Delete(IDelete)		(*models.Project, error)
+	}
+)
