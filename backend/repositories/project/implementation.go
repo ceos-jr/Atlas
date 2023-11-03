10141ca2a0bd3925b96332eb4ae10db153a11f1a
viquecastro <victoriacastro3108@gmail.com>
2023-11-03 11:38:16 -0300

    Adicionar funcoes update e delete

--- backend/repositories/project/implementation.go
+++ backend/repositories/project/implementation.go
@@ -47,3 +47,58 @@ func (r *Repository) Create(createData ICreate) (*models.Project, error){
 	
 	return &project, nil
 }
+
+
+///////////////////////////////////////////////////////////////////
+//  var updateData do tipo IUpdate
+//  
+func (r *Repository) Update(updateData IUpdate) (*models.Project, error) {
+	var project = models.Project{ID: updateData.ID}
+	var fieldMap = make(map[string]interface{})
+
+	if updateData.Name == nil &&
+		updateData.Sector == nil {
+			return nil, errors.New("No fields to update")
+		}
+	
+	if updateData.Name != nil {
+		if !r.ValidProjectName(*&updateData.Name) {
+			return nil, errors.New("Invalid Project Name")
+		}
+		fieldMap["project_name"] = *&updateData.Name
+	}
+
+	if updateData.Sector != nil {
+		fieldMap["sector_number"] = *&updateData.Sector
+	}
+
+	fieldMap["updated_at"] = time.Now()
+	result := r.GetDB().Model(&project).Updates(fieldMap)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &project, nil
+}
+
+func (r *Repository) Delete(deleteData IDelete) (*models.Project, error) {
+	var project = models.Project{ID: deleteData.ID}
+	verifyExistence := r.GetDB().First(&project)
+
+	if verifyExistence.Error != nil {
+		return nil, verifyExistence.Error
+	}
+
+	result := r.GetDB().Delete(&project)
+
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &project, nil
+}
+
+
+// Criar uma função pra validar o número do setor 
+                                                                                                                                                                                                                                                                                                       
