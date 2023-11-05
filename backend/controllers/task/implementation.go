1fa9d5406ce74c260c4d8a55291ef039f6ca603b
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2023-11-05 12:26:44 -0300

    Update implementation.go

--- backend/controllers/task/implementation.go
+++ backend/controllers/task/implementation.go
@@ -9,6 +9,48 @@ type CreateUserRequestBodyId struct {
 	AssignedTo  uint `json:"assigned_to" validate:"required"`
 }
 
+func (handler *BaseHandler) MarkAsCompleted(context *fiber.Ctx) error {
+	body := new(CreateUserRequestBodyId)
+
+	if parseError := context.BodyParser(body); parseError != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid request body",
+			"error":   parseError.Error(),
+		})
+	}
+
+	validationErrors := handler.Validator.Validate(body)
+
+	if validationErrors != nil {
+		errorMessages := make([]string, len(validationErrors))
+
+		for index := range validationErrors {
+			errorMessages[index] = validationErrors[index].Message
+		}
+
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid request body",
+			"errors":  errorMessages,
+		})
+	}
+
+	newTask, serviceError := handler.Service.MarkTaskAsCompleted(
+		body.Id,
+	)
+
+	if serviceError != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Task updating error",
+			"error":   serviceError.Error(),
+		})
+	}
+
+	return context.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "Task marked as concluded successfully",
+		"task":    newTask,
+	})
+}
+
 func (handler *BaseHandler) AssignedTo(context *fiber.Ctx) error {
 	body := new(CreateUserRequestBodyId)
 
