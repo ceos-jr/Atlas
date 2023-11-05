2ceb390180d7118ad05916329bdce6aa933dd91f
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2023-11-05 12:37:36 -0300

    Update task_service_test.go

--- backend/tests/integration/services/task/task_service_test.go
+++ backend/tests/integration/services/task/task_service_test.go
@@ -1,10 +1,19 @@
 package taskservicetest
 
 import (
-
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
+
+func (suite *TestSuit) TestConcludedTask() {
+	NewStatus := uint(1)
+
+	UpdateStatus, UpdateErr := suite.TaskService.MarkTaskAsCompleted(suite.MockTasks[0].ID)
+  
+	suite.Nil(UpdateErr, "Update error must be nil")
+
+	suite.Equal(NewStatus, UpdateStatus.Status)
+}
 
 func (suite *TestSuit) TestAssignTask() {
 
