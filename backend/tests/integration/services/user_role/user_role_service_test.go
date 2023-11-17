0fa5d8f4cc7595f7f59b8aef01da4ae55dea3f40
lui5085 <121206535+lui5085@users.noreply.github.com>
2023-11-17 21:11:27 +0000

    add:testes assignerole

--- backend/tests/integration/services/user_role/user_role_service_test.go
+++ backend/tests/integration/services/user_role/user_role_service_test.go
@@ -36,3 +36,24 @@ func (suite *TestSuite) TestUnassignRole() {
 func TestUserRoleRepository(test *testing.T) {
 	suite.Run(test, new(TestSuite))
 }
+
+func (suite *TestSuite) TestAssigneRole(){
+	UserRole, err := suite.Service.AssigneRole(suite.MockUsers[0].ID, suite.MockRoles[0].ID)
+
+	suite.Nil(err, "Assigne Role error must be nil")
+	suite.Equal(suite.MockUsers[0].ID, UserRole.UserID)
+	suite.Equal(suite.MockRoles[0].ID, UserRole.RoleID)
+}
+
+func (suite *TestSuite) TestAssigneRoleErr() {
+	Invalid_UserID := uint(500)
+	Invalid_RoleID := uint(500)
+
+	_, err := suite.Service.AssigneRole(Invalid_UserID, suite.MockRoles[0].ID,)
+	suite.Equal("invalid user id", err.Error(), "Expected to have an error")
+
+	_, err = suite.Service.UnassignRole(suite.MockUsers[0].ID,Invalid_RoleID,)
+	suite.Equal("invalid role id", err.Error(), "Expected to have an error")
+
+}
+
