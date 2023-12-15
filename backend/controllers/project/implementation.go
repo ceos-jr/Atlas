c0551867fe06b648d8f7163def83799cfb82fd9d
lui5085 <121206535+lui5085@users.noreply.github.com>
2023-12-15 17:32:48 +0000

    fix: controllers

--- backend/controllers/project/implementation.go
+++ backend/controllers/project/implementation.go
@@ -1,27 +1,30 @@
 package project
 
 import (
+	"orb-api/services/project"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 type CreateProjectRequestBody struct {
-	Name: 		string `json:"name" validate:"required"`
-	SectorID:	uint`json:"sectorid" validate:"required"`
-	AdmID:		uint`json:"admid" validate:"required"`
+	ProjectID	uint`json:"projectid" validate:"required"`
+	Name 		string `json:"name" validate:"required"`
+	SectorID	uint`json:"sectorid" validate:"required"`
+	AdmID		uint`json:"admid" validate:"required"`
 }
 
 type CreateUserProjectRequestBody struct {
-	ProjectID:	uint`json:"projectid" validate:"required"`
-	UserID:	uint`json:"userid" validate:"required"`
+	ProjectID	uint`json:"projectid" validate:"required"`
+	UserID	uint`json:"userid" validate:"required"`
 }
 
 type CreateTaskProjectRequestBody struct {
-	ProjectID:	uint`json:"projectid" validate:"required"`
-	TaskID:	uint`json:"taskid" validate:"required"`
+	ProjectID	uint`json:"projectid" validate:"required"`
+	TaskID	uint`json:"taskid" validate:"required"`
 }
 
 type CreateSortTaskRequestBody struct {
-	ProjectID:	uint`json:"projectid" validate:"required"`
+	ProjectID	uint`json:"projectid" validate:"required"`
 }
 
 func (handler *BaseHandler) CreateProject(context *fiber.Ctx) error{
@@ -94,8 +97,8 @@ func (handler *BaseHandler) AssignUser(context *fiber.Ctx) error{
 	}
 
 	userassign, serviceError := handler.Service.AssignUser(
-		body.ProjectID
-		body.UserID
+		body.ProjectID,
+		body.UserID,
 	)
 
 	if serviceError != nil {
@@ -137,8 +140,8 @@ func (handler *BaseHandler) AssignTask(context *fiber.Ctx) error{
 	}
 
 	taskassign, serviceError := handler.Service.AssignTask(
-		body.ProjectID
-		body.UserID
+		body.ProjectID,
+		body.UserID,
 	)
 
 	if serviceError != nil {
@@ -154,7 +157,7 @@ func (handler *BaseHandler) AssignTask(context *fiber.Ctx) error{
 	})
 }
 
-func (handler *BaseHandler) AssignTask(context *fiber.Ctx) error{
+func (handler *BaseHandler) SortByDeadline(context *fiber.Ctx) error{
 	body := new(CreateUserProjectRequestBody)
 
 	if parseError := context.BodyParser(body); parseError != nil {
@@ -180,7 +183,7 @@ func (handler *BaseHandler) AssignTask(context *fiber.Ctx) error{
 	}
 
 	sortedtask, serviceError := handler.Service.SortByDeadline(
-		body.ProjectID
+		body.ProjectID,
 	)
 
 	if serviceError != nil {
@@ -196,7 +199,7 @@ func (handler *BaseHandler) AssignTask(context *fiber.Ctx) error{
 	})
 }
 
-func (handler *BaseHandler) CreateProject(context *fiber.Ctx) error{
+func (handler *BaseHandler) UpdateProject(context *fiber.Ctx) error{
 	body := new(CreateProjectRequestBody)
 
 	if parseError := context.BodyParser(body); parseError != nil {
@@ -221,13 +224,12 @@ func (handler *BaseHandler) CreateProject(context *fiber.Ctx) error{
 		})
 	}
 
-	updateProject, serviceError := handler.Service.UpdateProject(Update{
+	updateProject, serviceError := handler.Service.UpdateProject(project.Update{
 		ID: body.ProjectID,
-		Name: body.Name,
-		Sector: body.SectorID,
-		AdmID: body.AdmID,
-	}
-	)
+		Name: &body.Name,
+		Sector: &body.SectorID,
+		AdmID: &body.AdmID,
+	})
 
 	if serviceError != nil {
 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
