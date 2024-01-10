d992189ddfeec27d84750b3f3fda66041da402fd
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2024-01-10 18:29:42 -0300

    Criar router

--- backend/server.go
+++ backend/server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"orb-api/routes"
 	"log"
 	"orb-api/config"
 	"orb-api/controllers"
@@ -10,20 +11,21 @@ import (
 
 func main() {
 	server := fiber.New()
-
 	repository, setupError := config.SetupDB(".env")
 
 	if setupError != nil {
 		log.Fatal(setupError)
 	}
-
 	defer config.CloseDB(repository)
 
 	services := services.SetupServices(repository)
 
 	controllers := controllers.SetupControllers(services)
 
-	server.Post("/register", controllers.User.CreateUser)
+	server.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Chablau!!")
+	})
 
+	routes.Setup(server, controllers)
 	server.Listen(":8000")
 }
