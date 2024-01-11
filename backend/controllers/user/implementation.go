5f8850a0686deebb3421c63b2abc3674a3bf267c
lui5085 <121206535+lui5085@users.noreply.github.com>
2024-01-11 18:02:56 -0300

    175 feat desabilitardeletar usuarios service controller (#181)

--- backend/controllers/user/implementation.go
+++ backend/controllers/user/implementation.go
@@ -2,6 +2,7 @@ package user
 
 import (
 	"orb-api/repositories/user"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -105,57 +106,9 @@ func (handler *BaseHandler) CreateUser(context *fiber.Ctx) error {
 // 	})
 // }
 
-// func (handler *BaseHandler) UpdateUser(context *fiber.Ctx) error {
-// 	body := new(CreateUserRequestBody)
-
-// 	if parseError := context.BodyParser(body); parseError != nil {
-// 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": "Invalid request body",
-// 			"error":   parseError.Error(),
-// 		})
-// 	}
-
-// 	validationErrors := handler.Validator.Validate(body)
-
-// 	if validationErrors != nil {
-// 		errorMessages := make([]string, len(validationErrors))
-
-// 		for index := range validationErrors {
-// 			errorMessages[index] = validationErrors[index].Message
-// 		}
-
-// 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": "Invalid request body",
-// 			"errors":  errorMessages,
-// 		})
-// 	}
-
-// 	UpdateUser, serviceError := handler.Service.UpdateUser(user.IUpdate{
-// 		ID: body.ID,
-// 		Name: &body.Name,
-// 		Email: &body.Email,
-// 		Password: &body.Password,
-// 		Status: &body.Status,
-	
-// 	})
-		
-
-// 	if serviceError != nil {
-// 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-// 			"message": "user update error",
-// 			"error":   serviceError.Error(),
-// 		})
-// 	}
-
-// 	return context.Status(fiber.StatusCreated).JSON(fiber.Map{
-// 		"message": "user update successfully",
-// 		"user":    UpdateUser,
-// 	})
-// }
-
-func (handler *BaseHandler) DeleteUser(context *fiber.Ctx) error {
+func (handler *BaseHandler) UpdateUser(context *fiber.Ctx) error {
+	// Parse do corpo da solicitação para obter os dados de atualização
 	body := new(CreateUserRequestBody)
-
 	if parseError := context.BodyParser(body); parseError != nil {
 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Invalid request body",
@@ -164,34 +117,75 @@ func (handler *BaseHandler) DeleteUser(context *fiber.Ctx) error {
 	}
 
 	validationErrors := handler.Validator.Validate(body)
-
 	if validationErrors != nil {
 		errorMessages := make([]string, len(validationErrors))
-
 		for index := range validationErrors {
 			errorMessages[index] = validationErrors[index].Message
 		}
-
 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "Invalid request body",
 			"errors":  errorMessages,
 		})
 	}
 
-	DeleteUser, serviceError := handler.Service.DeleteUser(
-		body.ID,
-	)
+	updateParams := user.IUpdate{
+		ID: body.ID,
+	}
 
-	if serviceError != nil {
+	if body.Email != "" {
+		handler.Service.UpdateEmail(updateParams.ID, body.Email)
+	}
+
+	if body.Status != 0 {
+		handler.Service.UpdateStatus(updateParams.ID, body.Status)
+	}
+
+	if body.Password != "" {
+		handler.Service.UpdatePassword(updateParams.ID, body.Password)
+	}
+
+	if body.Name != "" {
+		handler.Service.UpdateName(updateParams.ID, body.Name)
+	}
+
+	return context.Status(fiber.StatusOK).JSON(fiber.Map{
+		"message": "User updated successfully",
+		"user":    updateParams, 
+	})
+}
+
+
+
+func (handler *BaseHandler) DeleteUser(context *fiber.Ctx) error {
+	userIDstr := context.Params("id")
+
+	userIDint, err := strconv.Atoi(userIDstr)
+	if err != nil {
 		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"message": "user delete error",
+			"message": "Invalid user ID",
+			"error":   err.Error(),
+		})
+	}
+
+	if userIDint < 0 {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "Invalid user ID",
+		})
+	}
+
+	userID := uint(userIDint)
+
+	deletedUser, serviceError := handler.Service.DeleteUser(userID)
+	if serviceError != nil {
+		return context.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"message": "Error deleting user",
 			"error":   serviceError.Error(),
 		})
 	}
 
 	return context.Status(fiber.StatusCreated).JSON(fiber.Map{
 		"message": "user deleted successfully",
-		"user":    DeleteUser,
+		"user":    deletedUser,
 	})
 }
 
