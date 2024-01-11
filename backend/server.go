1cffa6ebc761f2af71fecf94b19fd57cffeba8f9
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2024-01-10 23:43:54 -0300

    Revert "Criar router"
    This reverts commit d992189ddfeec27d84750b3f3fda66041da402fd.

--- backend/server.go
+++ backend/server.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"orb-api/routes"
 	"log"
 	"orb-api/config"
 	"orb-api/controllers"
@@ -11,21 +10,20 @@ import (
 
 func main() {
 	server := fiber.New()
+
 	repository, setupError := config.SetupDB(".env")
 
 	if setupError != nil {
 		log.Fatal(setupError)
 	}
+
 	defer config.CloseDB(repository)
 
 	services := services.SetupServices(repository)
 
 	controllers := controllers.SetupControllers(services)
 
-	server.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Chablau!!")
-	})
+	server.Post("/register", controllers.User.CreateUser)
 
-	routes.Setup(server, controllers)
 	server.Listen(":8000")
 }
