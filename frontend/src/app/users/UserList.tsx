87fce448cfd5f76c4457e59c591a4651af2fc757
Alan <alanzcc1@gmail.com>
2024-01-28 13:28:16 -0300

    Responsividade adicionada

--- frontend/src/app/users/UserList.tsx
+++ frontend/src/app/users/UserList.tsx
@@ -10,25 +10,25 @@ import { IoMdClose } from "react-icons/io";
 type UserCardProps = { user: string };
 function UserCard({ user }: UserCardProps) {
   return (
-    <div className="flex m-2 max-h-14 justify-evenly rounded  bg-slate-400 text-black font-sans  text-center text-md">
-      <CgProfile className="m-3 text-black bg-white rounded-full  " />
+    <div className="flex m-2 max-h-14 lg:max-h-24 2xl:max-h-32 justify-evenly rounded  bg-slate-400 text-black font-sans  text-center text-md">
+      
+      <CgProfile className="m-3 md:m-2 lg:my-4 md:w-10 lg:w-12 xl:w-16 2xl:w-20 md:h-10 lg:h-12 xl:h-16 2xl:h-20 text-black bg-white rounded-full  " />
 
-      <div className="flex-col my-2">
-        <h1 className="text-sm font-bold text-left">{user}</h1>
-        <div className="flex text-sm space-x-2">
+      <div className="flex-col my-4 md:my-2.5 lg:my-4">
+        <h1 className="text-sm lg:text-lg xl:text-xl 2xl:text-3xl font-bold  text-left">{user}</h1>
+        <div className="flex text-sm lg:text-base xl:text-xl 2xl:text-3xl space-x-2">
           <a className="text-orange-700 ">Assessor</a>
-          <Link href="">Relatório</Link>
+          <Link href="" className="hover:text-blue-600">Relatório</Link>
         </div>
       </div>
 
-      <FaWhatsapp className=" my-3 p-1 text-white bg-green-500 rounded-full" />
+      <FaWhatsapp  className=" my-3 md:my-2 lg:my-4 xl:my-5 p-1 md:w-10 lg:w-12 xl:w-14 2xl:w-20 md:h-10 lg:h-12 xl:h-14 2xl:h-20 text-white bg-green-500 rounded-full" />
 
       <a
         href=""
-        className="flex min-w-0 max-w-10 p-2 mx-4 my-2.5 space-x-1  bg-white text-sm"
-      >
-        <IoMdClose className="my-1" />
-        <button>Desabilitar</button>
+        className="flex min-w-0 max-w-10 lg:w-28 xl:w-32 2xl:w-52 lg:h-14 2xl:h-20 p-2 mx-4 my-2.5 lg:my-3.5 xl:my-5  space-x-1 bg-white hover:bg-red-600">
+        <IoMdClose className="my-1 md:my-0 xl:-my-1 2xl:my-1 md:w-6 lg:w-8 xl:w-12 2xl:w-16 md:h-6 lg:h-8 xl:h-12 2xl:h-16 " />
+        <button className="text-base xl:text-lg 2xl:text-3xl">Desabilitar</button>
       </a>
     </div>
   );
@@ -70,19 +70,19 @@ export default function UserList() {
   }
 
   return (
-    <div className="flex-col">
-      <div className="flex justify-evenly">
+    <div className="flex-col p-2 2xl:p-6">
+      <div className="flex justify-evenly md:justify-start">
         <input
           placeholder="Pesquisar usuários"
           value={state}
           onChange={handleChange}
-          className="mx-2 my-4 p-2 w-64  text-black text-sm border-2 rounded-md border-black"
+          className="mx-2 my-4 p-2 w-60 lg:w-80 2xl:w-[20%] 2xl:h-16 text-black text-sm lg:text-lg 2xl:text-3xl border-2 rounded-md border-black"
         ></input>
-        <button className="my-4 p-2 text-black text-sm border-2 rounded-md bg-blue-500">
+        <button className="my-4 p-2 lg:w-28 xl:w-32 2xl:w-48 2xl:h-16 text-black text-sm lg:text-lg 2xl:text-3xl border-2 rounded-md bg-blue-500 hover:bg-blue-700">
           Adicionar
         </button>
       </div>
-      <div className=" max-h-96 scroll-smooth overflow-y-scroll">
+      <div className="max-h-96 lg:max-h-[35rem] 2xl:max-h-[72rem] scroll-smooth overflow-y-scroll bg-white">
         <Listing state={state} />
       </div>
     </div>
