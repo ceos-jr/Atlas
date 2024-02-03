92ff0767b59db374b0cb9ee62029c39c8d97dd85
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2024-02-03 00:10:30 +0000

    Changes to be committed: 	modified:   frontend/src/app/globals.css 	modified:   frontend/src/app/login/page.tsx

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
@@ -10,23 +10,42 @@ const LoginPage = () => {
     router.push("/");
   };
 
+  const [searchString, setSearchString] = useState({
+    name: "",
+    email: "",
+    whatsapp: "",
+  });
+
   return (
-    <div className="flex justify-center items-center h-screen text-center font-quick">
-      <div className="card bg-white rounded-lg shadow-md p-6">
-        <h1 className="text-3xl font-semibold mb-4 text-primary-blue">
-          Teste Login
-        </h1>
-        <p className="text-lg text-gray-600">
-          Uma plataforma para gerenciamento de demandas e equipes.
-        </p>
-        <button
-          onClick={goHomePage}
-          className='className="px-4 py-2 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-xs"'
-        >
-          Home
-        </button>
+    <div className="flex flex-col md:flex-row justify-center md:items-center h-screen text-center font-quick mx-4 md:mx-8 lg:mx-12">
+        <p className="text-gray-700 text-3xl my-12">cadastro de usuário</p>
+        <div className="card flex flex-col bg-gray-200 rounded-lg shadow-xl px-6 py-2">
+          <input
+            type="text"
+            placeholder="Nome Completo"
+            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            onChange={(e) => setSearchString({ ...searchString, name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            onChange={(e) => setSearchString({ ...searchString, email: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Whatsapp"
+            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            onChange={(e) => setSearchString({ ...searchString, whatsapp: e.target.value })}
+          />
+          <button
+            onClick={goHomePage}
+            className='px-4 py-2 w-1/3 mx-auto my-7 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-sm text-shadow'
+          >
+            Home
+          </button>
+        </div>
       </div>
-    </div>
   );
 };
 
