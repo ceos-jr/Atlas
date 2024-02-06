4b9252df421af24e7464e05b4d40d9ddec8e5601
lui5085 <luis.a.albuq@outlook.com>
2024-02-06 17:54:23 -0300

    inicio

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -11,21 +11,37 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen text-center font-quick">
-      <div className="card bg-white rounded-lg shadow-md p-6">
-        <h1 className="text-3xl font-semibold mb-4 text-primary-blue">
-          Teste Login
+    <div className="bg-white flex justify-center items-center h-screen text-center font-quick">
+      <div className="absolute inset-x-0 top-10">
+        <h1 className="text-black">
+          login de usuário
         </h1>
-        <p className="text-lg text-gray-600">
-          Uma plataforma para gerenciamento de demandas e equipes.
-        </p>
-        <button
-          onClick={goHomePage}
-          className='className="px-4 py-2 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-xs"'
-        >
-          Home
-        </button>
       </div>
+      <form className="bg-neutral-100 flex flex-col items-center justify-center mx-4  sm:w-2/3 md:w-1/2 lg:w-1/3 rounded-lg p-8 ">
+        <label className="mb-4 w-full">
+          <input 
+            type="text" 
+            placeholder="Email"
+            className=" p-4 border border-gray-300 rounded-lg w-full text-base text-zinc-500"
+          />
+        </label>
+        <label className="mb-4 w-full">
+          <input 
+            type="text" 
+            placeholder="Senha"
+            className=" p-4 border border-gray-300 rounded-lg w-full mt-5 text-base text-black"
+          />
+        </label>
+        <button 
+          type="submit" 
+          className="font-def bg-sky-600 text-white p-1 rounded-lg hover:bg-cyan-700 w-1/2 mt-5 mb-6 text-lg "
+        >
+          entrar  
+        </button>
+
+          
+      </form>
+      
     </div>
   );
 };
