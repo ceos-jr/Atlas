522699360dcea981ca2e4900143fa32911481ccb
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2024-02-11 23:02:15 +0000

    modified:   frontend/src/app/globals.css 	modified:   frontend/src/app/login/page.tsx 	modified:   frontend/tailwind.config.ts

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -18,31 +18,31 @@ const LoginPage = () => {
 
   return (
     <div className="flex flex-col md:flex-row justify-center md:items-center h-screen text-center font-quick mx-4 md:mx-8 lg:mx-12">
-        <p className="text-gray-700 text-3xl my-12">cadastro de usuário</p>
-        <div className="card flex flex-col bg-gray-200 rounded-lg shadow-xl px-6 py-2">
+        <p className="text-gray-700 text-3xl my-12 color-[#727272]">cadastro de usuário</p>
+        <div className="card flex flex-col bg-[#f4f9ff] rounded-lg shadow-[0px_4px_4px_0px_#00000040] px-6 py-4 mx-6">
           <input
             type="text"
             placeholder="Nome Completo"
-            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            className="form-input rounded-lg my-3 py-3 px-2 text-base border border-gray-400"
             onChange={(e) => setSearchString({ ...searchString, name: e.target.value })}
           />
           <input
             type="text"
             placeholder="Email"
-            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            className="form-input rounded-lg my-3 py-3 px-2 text-base border border-gray-400"
             onChange={(e) => setSearchString({ ...searchString, email: e.target.value })}
           />
           <input
             type="text"
             placeholder="Whatsapp"
-            className="form-input rounded-lg border-2 my-2 py-3 px-2 text-base border border-gray-400"
+            className="form-input rounded-lg my-3 py-3 px-2 text-base border border-gray-400"
             onChange={(e) => setSearchString({ ...searchString, whatsapp: e.target.value })}
           />
           <button
             onClick={goHomePage}
-            className='px-4 py-2 w-1/3 mx-auto my-7 bg-primary-blue text-white rounded hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-sm text-shadow'
+            className='px-4 py-2 w-2/5 mx-auto my-7 bg-[#ff8a00] text-white rounded-[10px] hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300 text-sm text-shadow-[0px_4px_4px_0px_#00000040]'
           >
-            Home
+            Cadastrar
           </button>
         </div>
       </div>
--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
       },
       fontFamily: {
         'quick' : ['Quicksand', 'sans-serif']
-      }
+      },
     },
   },
   plugins: [],
