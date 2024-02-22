db659a0eab7a8b19ebb917855259f668e01a69c5
Julia Naomi <julianaomitakimaki@gmail.com>
2024-02-22 15:57:14 -0300

    Esqueleto, falta mapeamento, responsividade etc

--- frontend/src/app/homepage/page.tsx
+++ frontend/src/app/homepage/page.tsx
@@ -5,13 +5,50 @@ export default function Page() {
     return (
     
 <div className="grid grid-cols-3 auto-rows-[300px] gap-4">
-    <div className="p-2 flex flex-col items-center justify-center col-span-2 row-[200px]">3</div>
-    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center col-span-1.5">4</div>
-    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">5</div>
-    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">6</div>
-    <div className="bg-neutral-100 border-2 rounded-xl row-span-3 p-2 flex flex-col items-center justify-center">7</div>
-    <div className="bg-neutral-100 border-2 rounded-xl row-span-2 col-span-2 p-2 flex flex-col items-center justify-center">8</div>
+    <div className="p-2 flex items-center col-span-2">
+      <div className="m-4 w-36 h-36 bg-white shadow-lg rounded-full ">
+      </div>
+      <div className="ml-4">
+      <h2 className="text-2xl font-bold">Your Text Here</h2>
+      <p>Your additional content goes here.</p>
+    </div>
+    </div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center ">
+    <div className="m-4 w-44 h-44 bg-white border border-blue-600 shadow-lg rounded-full flex items-center justify-center">
+      <h1 className="text-black">100</h1>
+    </div>
+    </div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">
+    <div className="ml-4">
+      <h2 className="text-2xl font-bold ">Your Text Here</h2>
+      <p>Your additional content goes here.</p>
+    </div>
+    </div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">
+    <p>Your additional content goes here.</p>
+    <p>Your additional content goes here.</p>
+    <p>Your additional content goes here.</p>
+    </div>
+    <div className="bg-neutral-100 border-2 rounded-xl row-span-3 p-4 py-8 flex flex-col items-center gap-6 ">
+    <div className=" w-full h-36 p-2 bg-white rounded-md shadow-md"/>
+    <div className=" w-full h-36 p-2 bg-white rounded-md shadow-md"/>
+    <div className=" w-full h-36 p-2 bg-white rounded-md shadow-md"/>
+    <div className=" w-full h-36 p-2 bg-white rounded-md shadow-md"/>
+    <div className=" w-full h-36 p-2 bg-white rounded-md shadow-md"/>
+
+    </div>
+    <div className="bg-neutral-100 border-2 rounded-xl row-span-2 col-span-2 p-6 flex flex-col gap-4 ">
+      <div className=" w-full h-12 p-4 bg-neutral-100 rounded-md shadow-md flex">
+        <h1 className="text-2xl font-bold ">Your Text Here</h1>
+      </div>
+      <div className=" w-full h-12 p-2 bg-neutral-100 rounded-md hover: shadow-md"/>
+      <div className=" w-full h-12 p-2 bg-neutral-100 rounded-md shadow-md"/>
+      <div className=" w-full h-12 p-2 bg-neutral-100 rounded-md shadow-md"/>
+      <div className=" w-full h-12 p-2 bg-neutral-100 rounded-md shadow-md"/>
+
+    </div>
 </div>
+
     )
-  }
+};
 
--- frontend/src/app/ui/sidebar.tsx
+++ frontend/src/app/ui/sidebar.tsx
@@ -2,92 +2,68 @@ import React from 'react';
 
 const SideBar = () => {
   return (
-   <div className="grid grid-rows-7 grid-flow-col gap-4">
-      <nav className = "flex flex-col h-full px-2 py-4 order-r dark:border-indigo-800">
-        <div className="flex flex-col items-center justify-center flex-1 space-y-8">
-            <a
-            href="#"
-            className="p-2 text-white hover:text-indigo-600 hover:bg-indigo-100"
-            >
-            <span className="sr-only">Home</span>
-            <svg
-                className="w-7 h-7"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-                />
-            </svg>
-            </a>
+    
+    <div
+        className="relative pattern"
+    >
+        <nav
+        className="z-20 flex shrink-0 grow-0 justify-around gap-4 border-t border-gray-200 bg-white/50 p-8 shadow-lg m-4 backdrop-blur-lg bg-neutral-200 fixed top-2/4 -translate-y-2/4 left-6 min-h-[auto] min-w-[64px] flex-col rounded-lg border"
+        >
+        <a
+            href="#logo"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 bg-indigo-50 text- dark:bg-sky-900 dark:text-sky-50"
+        >
+            <small className="text-center text-xs font-medium"> </small>
 
-            <a
-            href="#"
-            className="p-2 text-indigo-400 bg-indigo-50"
-            >
-            <span className="sr-only">Messages</span>
-            <svg
-                className="w-7 h-7"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
-                />
-            </svg>
-            </a>
-            <a
-            href="#"
-            className="p-2 text-indigo-400 "
-            >
-            <span className="sr-only">Another Link</span>
-            <svg
-                class="w-7 h-7"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
-                />
-            </svg>
-            </a>
-            <a
-            href="#"
-            className="p-2 text-indigo-400"
-            >
-            <span classNmame="sr-only">Users</span>
-            <svg
-                class="w-7 h-7"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-                <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-                />
-            </svg>
-            </a>
-        </div>
-          </nav>
+        </a>
+    
+        <a
+            href="#homepage"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+    
+            <small className="text-center text-xs font-medium"> Home </small>
+        </a>
+    
+        <a
+            href="#meuperfil"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+           
+            <small className="text-center text-xs font-medium"> Meu Perfil </small>
+        </a>
+    
+        
+    
+        <a
+            href="#equipes"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+            <small className="text-xs font-medium">Equipes</small>
+        </a>
+
+        <a
+            href="#meuspontos"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+           
+            <small className="text-center text-xs font-medium"> Meus pontos </small>
+        </a>
+        <a
+            href="#config"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+           
+            <small className="text-center text-xs font-medium"> Configurações </small>
+        </a>
+        <a
+            href="#logout"
+            className="flex aspect-square min-h-[32px] w-16 flex-col items-center justify-center gap-1 rounded-md p-8 text-gray-700 hover:bg-blue-300 shadow-md"
+        >
+           
+            <small className="text-center text-xs font-medium"> Sair </small>
+        </a>
+        </nav>
     </div>
   );
 };
