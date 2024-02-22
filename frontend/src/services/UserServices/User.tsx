c80b4a90744533a4c64c48f2e13817fd395f1e80
Pedro Wagner Rabelo Evaristo <130172864+porcotrante@users.noreply.github.com>
2024-02-22 16:43:41 -0300

    Update User.tsx

--- frontend/src/services/UserServices/User.tsx
+++ frontend/src/services/UserServices/User.tsx
@@ -15,3 +15,25 @@ export async function deleteUser(userId: string) {
       return { ok: false, err: error };
     }
 }
+export async function createUser(userId: string, userName: string, userEmail: string, userStatus: string) {
+  try{
+    const path = "register";
+    const data = {
+      ID: userId,
+      name: userName,
+      email: userEmail,
+      status: userStatus,
+    }
+
+    const response = await apiProvider.post<{message: string}, typeof data>(path, data);
+
+    console.log(response);
+    console.log("Usuário cadastrado com sucesso!");
+
+    return {ok: true, data: response};
+  } catch (error) {
+    console.error("Erro ao criar usuário: ", error);
+
+    return {ok: false, err: error};
+  }
+}
