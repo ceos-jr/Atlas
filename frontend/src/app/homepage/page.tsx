06cdaf019a6429ac38905e88701c4a30bb967f10
Julia Naomi <julianaomitakimaki@gmail.com>
2024-02-22 08:37:12 -0300

    Layout homepage esqueleto

--- frontend/src/app/homepage/page.tsx
+++ frontend/src/app/homepage/page.tsx
@@ -1,8 +1,17 @@
 "use client"
-
 import React from "react";
 
 export default function Page() {
-    return <p>HomePage</p>;
+    return (
+    
+<div className="grid grid-cols-3 auto-rows-[300px] gap-4">
+    <div className="p-2 flex flex-col items-center justify-center col-span-2 row-[200px]">3</div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center col-span-1.5">4</div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">5</div>
+    <div className="bg-neutral-100 border-2 rounded-xl p-2 flex flex-col items-center justify-center">6</div>
+    <div className="bg-neutral-100 border-2 rounded-xl row-span-3 p-2 flex flex-col items-center justify-center">7</div>
+    <div className="bg-neutral-100 border-2 rounded-xl row-span-2 col-span-2 p-2 flex flex-col items-center justify-center">8</div>
+</div>
+    )
   }
 
--- frontend/src/app/ui/sidebar.tsx
+++ frontend/src/app/ui/sidebar.tsx
@@ -2,14 +2,92 @@ import React from 'react';
 
 const SideBar = () => {
   return (
-    <div className="flex flex-col h-screen bg-gray-800 text-white">
-      <div className="p-4 hover:bg-gray-700 cursor-pointer">
-        <i className="fas fa-user mr-2"></i> User
-      </div>
-      <div className="p-4 hover:bg-gray-700 cursor-pointer">
-        <i className="fas fa-home mr-2"></i> Home
-      </div>
-      {/* Add more items as needed */}
+   <div className="grid grid-rows-7 grid-flow-col gap-4">
+      <nav className = "flex flex-col h-full px-2 py-4 order-r dark:border-indigo-800">
+        <div className="flex flex-col items-center justify-center flex-1 space-y-8">
+            <a
+            href="#"
+            className="p-2 text-white hover:text-indigo-600 hover:bg-indigo-100"
+            >
+            <span className="sr-only">Home</span>
+            <svg
+                className="w-7 h-7"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                />
+            </svg>
+            </a>
+
+            <a
+            href="#"
+            className="p-2 text-indigo-400 bg-indigo-50"
+            >
+            <span className="sr-only">Messages</span>
+            <svg
+                className="w-7 h-7"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"
+                />
+            </svg>
+            </a>
+            <a
+            href="#"
+            className="p-2 text-indigo-400 "
+            >
+            <span className="sr-only">Another Link</span>
+            <svg
+                class="w-7 h-7"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 5a1 1 0 011-1h14a1 1 0 011 1v2a1 1 0 01-1 1H5a1 1 0 01-1-1V5zM4 13a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H5a1 1 0 01-1-1v-6zM16 13a1 1 0 011-1h2a1 1 0 011 1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-6z"
+                />
+            </svg>
+            </a>
+            <a
+            href="#"
+            className="p-2 text-indigo-400"
+            >
+            <span classNmame="sr-only">Users</span>
+            <svg
+                class="w-7 h-7"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+                />
+            </svg>
+            </a>
+        </div>
+          </nav>
     </div>
   );
 };
